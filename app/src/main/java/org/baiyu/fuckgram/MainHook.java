e6fe5cea970f6930b6980db8cca1d9a2239a67fc
lyaaz <45838887+lyaaz@users.noreply.github.com>
2023-03-10 21:42:10 +0800

    init

---
+++ app/src/main/java/org/baiyu/fuckgram/MainHook.java
@@ -0,0 +1,66 @@
+package org.baiyu.fuckgram;
+
+import android.app.Activity;import android.widget.Toast;
+import java.util.ArrayList;
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XC_MethodReplacement;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.XposedHelpers;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+public class MainHook implements IXposedHookLoadPackage {
+    private final static long DEFAULT_MAX_FILE_SIZE = 1024L * 1024L * 2001L;
+    private final static int downloadChunkSizeBig = 1024 * 1024;
+    private final static int maxDownloadRequests = 8;
+    private final static int maxDownloadRequestsBig = 8;
+    private final static int maxCdnParts = (int) (DEFAULT_MAX_FILE_SIZE / downloadChunkSizeBig);
+    @Override
+    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+        try {
+            XposedBridge.hookAllMethods(
+                    XposedHelpers.findClass(
+                            "org.telegram.messenger.MessagesController", lpparam.classLoader),
+                    "getSponsoredMessages",
+                    XC_MethodReplacement.returnConstant(null));
+            XposedBridge.hookAllMethods(
+                    XposedHelpers.findClass(
+                            "org.telegram.messenger.MessagesController", lpparam.classLoader),
+                    "isChatNoForwards",
+                    XC_MethodReplacement.returnConstant(false));
+            XposedBridge.hookAllMethods(
+                    XposedHelpers.findClass(
+                            "org.telegram.ui.ChatActivity", lpparam.classLoader),
+                    "addSponsoredMessages",
+                    XC_MethodReplacement.returnConstant(null));
+            XposedBridge.hookAllMethods(
+                    XposedHelpers.findClass(
+                            "org.telegram.messenger.MediaDataController", lpparam.classLoader),
+                    "getEnabledReactionsList",
+                    XC_MethodReplacement.returnConstant(new ArrayList<>()));
+            XposedBridge.hookAllMethods(
+                    XposedHelpers.findClass(
+                            "org.telegram.messenger.FileLoadOperation", lpparam.classLoader),
+                    "updateParams",
+                    new XC_MethodHook() {
+                        @Override
+                        protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                            super.beforeHookedMethod(param);
+                            XposedHelpers.setIntField(param.thisObject, "downloadChunkSizeBig", downloadChunkSizeBig);
+                            XposedHelpers.setObjectField(param.thisObject, "maxDownloadRequests", maxDownloadRequests);
+                            XposedHelpers.setObjectField(param.thisObject, "maxDownloadRequestsBig", maxDownloadRequestsBig);
+                            XposedHelpers.setObjectField(param.thisObject, "maxCdnParts", maxCdnParts);
+                            param.setResult(null);
+                        }
+                    });
+        } catch (Throwable t) {
+            XposedBridge.log(t);
+            XposedHelpers.findAndHookMethod(Activity.class, "onResume", new XC_MethodHook() {
+                @Override
+                protected void afterHookedMethod(MethodHookParam param) {
+                    Toast.makeText((Activity) param.thisObject, "FuckGram: Hook failed, check log for details", Toast.LENGTH_LONG).show();
+                }
+            });
+        }
+    }
+}
