39e9976102cbb7352886a811646a8c16fe796613
lyaaz <45838887+lyaaz@users.noreply.github.com>
2024-07-16 15:11:50 +0800

    chore: bump app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "org.lyaaz.fuckgram"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.1"
+        versionCode = 2
+        versionName = "1.2"
     }
 
     signingConfigs {
