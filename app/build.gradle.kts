fe4b5cc9d668e8d71df4c07c8f37d37423e4fec9
lyaaz <45838887+lyaaz@users.noreply.github.com>
2024-11-02 09:26:12 +0800

    chore: update compileSdk to 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ fun String.execute(currentWorkingDir: File = file("./")): String {
 
 android {
     namespace = "org.lyaaz.fuckgram"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "org.lyaaz.fuckgram"
